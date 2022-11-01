0a60fa14681125bcf7da8b43f5183a271738f5a2
Trung Nguyen <nghtrung1001@gmail.com>
2022-11-01 13:26:50 +0900

    Create brewingItemsData.js

---
+++ src/data/brewingItemsData.js
@@ -0,0 +1,71 @@
+const brewingItemsData = [
+  {
+    url:'https://www.starbucks.co.jp/info/comingsoon/info.html?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Holiday1_info_comingsoon_wb.jpg',
+    desc:'11.1(tue)から登場する商品をお知らせします。My Starbucks会員の方には詳細情報も。',
+    date:'2022.10.27'
+  },
+  {
+    url:'https://www.starbucks.co.jp/onlinestore/live2/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Holiday1_onlinestore_live2_wb.jpg',
+    desc:'11/1(火)開催！ホリデー気分を盛り上げるコーヒーの楽しみ方やグッズをLIVEでご紹介します。',
+    date:'2022.10.27'
+  },
+  {
+    url:'https://enjoystarbuckschilled.jp/meltycaramelbrulee.html?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_enjoystarbuckschilled_wb.jpg',
+    desc:'[新商品情報] スターバックス® チルドカップ「メルティキャラメルブリュレ with コーヒージェリー」が全国のファミリーマートで新発売',
+    date:'2022.10.18'
+  },
+  {
+    url:'https://www.starbucks.co.jp/reserve/roastery/onlinestore/coffee_beans/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_reserve_roastery_onlinestore_coffee_beansA_wb.jpg',
+    desc:'スターバックス リザーブ®に新商品が登場。オンラインストアでもご購入いただけます。 ',
+    date:'2022.10.14'
+  },
+  {
+    url:'https://www.starbucks.co.jp/rewards/5years_anniversary/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_rewards_5years_anniversaryA_wb.jpg',
+    desc:'[Starbucks® Rewards] 5周年の感謝を込めて、Bonus Starなどたくさんのプレゼントがもらえるパレードに参加しませんか。',
+    date:'2022.10.13'
+  },
+  {
+    url:'https://www.starbucks.co.jp/egift_halloween2022/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Halloween_eGiftA_wb.jpg',
+    desc:'新登場の動くフレームやハロウィンのスタンプなどで、この時期ならではのオリジナルeGiftを贈ってみませんか。',
+    date:'2022.10.12'
+  },
+  {
+    url:'https://product.starbucks.co.jp/tumblermug/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_goodsA_wb.jpg',
+    desc:'[新商品情報] ゴーストや黒猫をモチーフにしたハロウィンのグッズが登場',
+    date:'2022.10.12'
+  },
+  {
+    url:'https://www.starbucks.co.jp/onlinestore/gift/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_onlinestore_giftA_wb.jpg',
+    desc:'オンラインストアで贈るハロウィンギフト',
+    date:'2022.10.12'
+  },
+  {
+    url:'https://www.starbucks.co.jp/oos/huc/?nid=wh_00_pc#for_household',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/23Fall2_oos_huc_for_householdA_wb.jpg',
+    desc:'[新商品情報] スーパー・コンビニ・オンラインでご購入いただける商品に、ホワイト モカが新登場。',
+    date:'2022.10.05 '
+  },
+  {
+    url:'https://www.starbucks.co.jp/coffee/220901/?nid=wh_00_pc',
+    imgUrl:'https://www.starbucks.co.jp/assets/images/web2/images/wb/images/22Fall1_coffeeA_wb.jpg',
+    desc:'[季節のコーヒー] Feel the autumn in your coffee コーヒーの香りと彩り豊かな秋',
+    date:'2022.09.21 '
+  },
+]
+
+export default brewingItemsData
+
+// {
+//   imgUrl:'',
+//   url:'',
+//   desc:'',
+//   date:'',
+// }
